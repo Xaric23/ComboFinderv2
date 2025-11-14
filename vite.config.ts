5750b559a0e7bd81b43a7044d4d301906e70965e
Xaric23 <Xaric23@users.noreply.github.com>
2025-11-14 07:51:35 -0500

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/ComboFinderv2/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
